1c161a5fe017b7603b902c5cd239d6ed6f4a0049
hatzy <hchaconb@miumg.edu.gt>
2025-10-22 00:09:18 -0600

    Rutas

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Configuration\Routing;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: [
+            __DIR__.'/../routes/web.php',
+            __DIR__.'/../routes/auth.php',
+            __DIR__.'/../routes/admin.php',
+            __DIR__.'/../routes/offers.php',
+            __DIR__.'/../routes/profile.php',
+        ],
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
--- routes/admin.php
+++ routes/admin.php
@@ -1,15 +1,20 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\UserManagementController;
 
-Route::middleware(['auth', 'role:admin'])
+
+/*Route::middleware(['auth', 'role:admin'])
     ->prefix('admin')
     ->name('admin.')
     ->group(function () {
 
-        Route::resource('users', \App\Http\Controllers\Admin\UserManagementController::class)
-            ->only(['index', 'create', 'store']);
+       // Route::resource('users', \App\Http\Controllers\Admin\UserManagementController::class)
+        //    ->only(['index', 'create', 'store']);
 
         // Futuras rutas:
         // Route::resource('verifications', AdminVerificationController::class);
-    });
+    });*/
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+
+
