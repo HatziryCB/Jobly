d0685b3db195f246984ac984d9e7581060154b33
hatzy <hchaconb@miumg.edu.gt>
2025-11-03 17:49:28 -0600

    Centralizacion de rutas auth

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,6 @@ Route::get('/', function(){
     return view('home');
 })->name('home');
 
-Route::middleware('guest')->group(function () {
-    Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
-    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
-
-});
-
 Route::get('/offers', [OfferController::class, 'index'])->name('offers.index');
 
 Route::view('/about', 'about')->name('about');
