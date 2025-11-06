39778e393d578df0da5dd6be07140243c7fb6179
hatzy <hchaconb@miumg.edu.gt>
2025-11-06 11:53:43 -0600

    Chat interno por postulación.

--- routes/applications.php
+++ routes/applications.php
@@ -1,12 +1,16 @@
 <?php
 use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\CandidateController;
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/employee/{offer}/apply', [ApplicationController::class, 'store'])->name('applications.store');
     Route::get('/my-applications', [ApplicationController::class, 'index'])->name('employee.applications');
     Route::post('/applications/{application}/accept', [ApplicationController::class, 'accept'])->name('applications.accept');
+    Route::get('/chat/{user}', [MessageController::class, 'index'])->name('chat.show');
+    Route::post('/chat/{user}', [MessageController::class, 'send'])->name('chat.send');
+
 });
 
 Route::middleware(['auth', 'role:employer'])->group(function () {
---
+++ routes/chat.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\MessageController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/chat/{user}', [MessageController::class, 'index'])->name('chat.show');
+    Route::post('/chat/{user}', [MessageController::class, 'send'])->name('chat.send');
+});
