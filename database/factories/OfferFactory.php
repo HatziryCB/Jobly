de84d2bd42d34df373cfc42e2509f4fc939ba301
hatzy <hchaconb@miumg.edu.gt>
2025-11-08 13:33:10 -0600

    hecho esta, hecho esta!! hecho esta!!!

--- database/factories/OfferFactory.php
+++ database/factories/OfferFactory.php
@@ -13,33 +13,40 @@ class OfferFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->fake->randomElement([
+            'title' => fake()->randomElement([
                 'Jardinero por día',
                 'Ayudante de construcción',
                 'Servicio de limpieza doméstica',
                 'Pintor de interiores',
                 'Cuidador temporal',
             ]),
-            'description' => $this->fake->paragraph,
-            'requirements' => $this->fake->sentence,
-            'category' => $this->fake->randomElement(['Limpieza', 'Pintura', 'Mudanza', 'Electricidad']),
-            'location_text' => $this->fake->address,
-            'lat' => $this->fake->latitude(15.69, 15.74), // Puerto Barrios approx
-            'lng' => $this->fake->longitude(-88.61, -88.58),
-            'pay_min' => $this->fake->numberBetween(50, 150),
-            'pay_max' => $this->fake->numberBetween(151, 300),
-            'duration_unit' => $this->fake->randomElement(['horas', 'días', 'semanas', 'meses', 'hasta finalizar']),
+
+            'description' => fake()->paragraph(),
+            'requirements' => fake()->sentence(),
+
+            'category' => fake()->randomElement(['Limpieza', 'Pintura', 'Mudanza', 'Electricidad']),
+
+            'location_text' => fake()->address(),
+
+            'lat' => fake()->latitude(15.69, 15.74),
+            'lng' => fake()->longitude(-88.61, -88.58),
+
+            'pay_min' => fake()->numberBetween(50, 150),
+            'pay_max' => fake()->numberBetween(151, 300),
+
+            'duration_unit' => fake()->randomElement(['horas', 'días', 'semanas', 'meses', 'hasta finalizar']),
             'duration_quantity' => function (array $attributes) {
                 return $attributes['duration_unit'] === 'hasta finalizar'
                     ? null
                     : rand(1, 10);
             },
-            'employer_id' => fn() => User::whereHas('roles', fn($q) =>
+
+            // Usar empleadores ya seedados
+            'employer_id' => User::whereHas('roles', fn($q) =>
             $q->where('name', 'employer')
             )->inRandomOrder()->value('id'),
 
             'status' => 'open',
         ];
-
     }
 }
